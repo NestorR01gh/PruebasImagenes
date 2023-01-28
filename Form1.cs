4bb6edead14945e7120438685be7e440294971e4
NestorR01 <neroaz@floridauniversitaria.es>
2023-01-28 01:39:53 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using DBLibrary;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PreubasImagenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            PruebasContext context = new PruebasContext();
+            var fileBase64 = Convert.ToBase64String(File.ReadAllBytes("hola.txt"));
+            context.Pictures.Add(new Picture()
+            {
+               Base64String = fileBase64,
+               Title = "hola.txt"
+            });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
